02ba652797ce86cc0143e9f72f8bcadf3a29c0e5
stampedsteve <67400828+stampedsteve@users.noreply.github.com>
2020-07-08 16:18:10 -0400

    Finished lab 5 with Chelsea

--- src/main/java/com/codedifferently/stayready/Main.java
+++ src/main/java/com/codedifferently/stayready/Main.java
@@ -1,4 +1,65 @@
 package com.codedifferently.stayready;
 
 public class Main {
+    public double dollToEuro(double money){
+        double euro = money * .94;
+        return euro;
+    }
+
+   
+    public double euroToDoll(double money){
+        double dollar = money * (1/.94);
+        return dollar;
+    }
+
+    public double euroToPound(double money){
+        double pound = money * (0.82/0.94);
+        // pound/euro
+        return pound;
+    }
+
+    public double poundToRupee(double money){
+        // Rupee / pound
+        double Rupee = money * (68.32/0.82);
+        return Rupee;
+    }
+
+    public double rupeeToCan(double money){
+        double can = money * (1.32/68.32);
+        //Can / rupee
+        return can;
+    }
+
+    public double canToSing(double money){
+        //Sing / Can
+         double sing = money * (1.43/1.32);
+        return sing;
+        
+    }
+
+    public double singToFrank(double money){
+        // Frank / sing
+        double frank = money * (1.01/1.43);
+        return frank;
+    }
+
+    public double frankToRing(double money){
+        // Ring / frank
+        double ring = money * (4.47/1.01);
+        return ring;
+    }
+
+    public double ringToYen(double money){
+        // Yen / ring
+        double yen = money * (115.84/4.47);
+        return yen;
+    }
+
+    public double yenToYuan(double money){
+        double yuan = money *(6.92 /115.84);
+        // Yuan / Yen
+        return yuan;
+    }
+
+
 }
--- src/test/java/com/codedifferently/stayready/MainTest.java
+++ src/test/java/com/codedifferently/stayready/MainTest.java
@@ -1,4 +1,127 @@
 package com.codedifferently.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
 public class MainTest {
+
+    @Test
+    public void dollToEuroTest(){
+        //Given
+        Main doll = new Main();
+        double expected = doll.dollToEuro(1);
+        //when
+        double actual = .94; 
+        //Then
+        Assert.assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void euroToDollTest(){
+        //Given
+        Main euro = new Main();
+        double expected = euro.euroToDoll(.94);
+
+        //When
+        double actual= 1.00; 
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.01);
+         
+    }
+
+    @Test
+    public void euroToPoundTest(){
+        //Given
+        Main euro = new Main();
+        //When
+        double expected = euro.euroToPound(.94);
+        double actual = .82; // doll/1
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void poundToRupeeTest(){
+        //Given
+        Main pound = new Main();
+        //When
+        double expected = pound.poundToRupee(.82);
+        double actual = 68.32; // rupee/1
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void rupeeToCanTest(){
+        //Given
+        Main rupee = new Main();
+        //When
+        double expected = rupee.rupeeToCan(68.32);
+        double actual = 1.32; // can/1
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void canToSingTest(){
+        //Given
+        Main can = new Main();
+        //When
+        double expected = can.canToSing(1.32);
+        double actual = 1.43; // sing/1
+        //Then
+        Assert.assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void singToFrank(){
+        //Given
+        Main sing = new Main();
+        //When
+        double expected = sing.singToFrank(1.43);
+        double actual = 1.01; // doll/1
+        //Then
+        Assert.assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void frankToRing(){
+        //Given
+        Main sing = new Main();
+        //When
+        double expected = sing.frankToRing(1.01);
+        double actual = 4.47; // doll/1
+        //Then
+        Assert.assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void ringToYen(){
+        //Given
+        Main ring = new Main();
+        //When  
+        double expected = ring.ringToYen(4.47);
+        double actual = 115.84;
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void yenToYuan(){
+        //Given
+        Main yen = new Main();
+        //When
+        double expected = yen.yenToYuan(115.84);
+        double actual = 6.92;
+        //Then
+        Assert.assertEquals(expected, actual, 0.01);
+    }
+
 }
